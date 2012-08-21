d0becf7b6e86d1c9cbb8ed9d91ef7e957c057449
scribu <mail@scribu.net>
2012-08-21 20:58:09 +0300

    reorder functions

--- lisp.py
+++ lisp.py
@@ -1,40 +1,42 @@
-from stdlib import native_fn
-
-def is_list(token):
-    return type(token) in [list, tuple]
-
 def fn_if(cond, a, b):
     if eval(cond):
         return eval(a)
 
     return eval(b)
 
-# todo: distinguish between symbols and strings
-def eval(token):
-    if is_list(token):
-        if not token:
-            return token
+def fn_print(*args):
+    print ', '.join(str(arg) for arg in args)
 
-        if str == type(token[0]):
-            if 'if' == token[0]:
-                return fn_if(*token[1:])
+def fn_concat(*args):
+    return ''.join(str(arg) for arg in args)
 
-            if 'def' == token[0]:
-                return fn_def(*token[1:])
+native_fn = {
+    '>': lambda a, b: a > b,
+    '>=': lambda a, b: a >= b,
+    '<': lambda a, b: a < b,
+    '<=': lambda a, b: a <= b,
+    'eq': lambda a, b: a == b,
 
-            if token[0] in native_fn:
-                return native_fn_call(*token)
+    '+': lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
+    '/': lambda a, b: a / b,
+    '%': lambda a, b: a % b,
 
-            if token[0] in user_fn:
-                return user_fn_call(*token)
-
-        return [eval(arg) for arg in token]
-    else:
-        return token
+    'concat': fn_concat,
+    'print': fn_print,
+}
 
 user_fn = {}
 
-def fn_def(name, args, *body):
+def is_list(token):
+    return type(token) in [list, tuple]
+
+def native_fn_call(name, *args):
+    evald_args = [eval(arg) for arg in args]   # evaluate args before function body
+    return native_fn[name](*evald_args)
+
+def user_fn_def(name, args, *body):
     if name in native_fn:
         print "Error: can't redefine '%s'; native function." % name
         return False
@@ -43,10 +45,6 @@ def fn_def(name, args, *body):
         'args': args,
         'body': body
     }
-
-def native_fn_call(name, *args):
-    evald_args = [eval(arg) for arg in args]   # evaluate args before function body
-    return native_fn[name](*evald_args)
 
 def _bind_var(name, value, token):
     if is_list(token):
@@ -65,6 +63,38 @@ def user_fn_call(name, *args):
     i = 0
     for arg_name in fn['args']:
         body = _bind_var(arg_name, eval(args[i]), body)
-        i = i+1
+        i += 1
+
+    # # DEBUG
+    # if user_fn_call.calls > 20:
+    #     import sys
+    #     sys.exit()
+    # user_fn_call.calls += 1
 
     return eval(body)[-1]    # return value from last statement
+
+# DEBUG
+# user_fn_call.calls = 0
+
+# todo: distinguish between symbols and strings
+def eval(token):
+    if is_list(token):
+        if not token:
+            return token
+
+        if str == type(token[0]):
+            if 'if' == token[0]:
+                return fn_if(*token[1:])
+
+            if 'def' == token[0]:
+                return user_fn_def(*token[1:])
+
+            if token[0] in native_fn:
+                return native_fn_call(*token)
+
+            if token[0] in user_fn:
+                return user_fn_call(*token)
+
+        return [eval(arg) for arg in token]
+    else:
+        return token
