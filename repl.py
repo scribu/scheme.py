4852864eb7c4838c2aa6997efd920747ca83e866
scribu <mail@scribu.net>
2012-11-18 20:33:22 +0200

    first pass at repl logic

--- repl.py
+++ repl.py
@@ -1,5 +1,13 @@
-import readline, core
+import core, lexer
 
 def start():
     scope = core.GlobalScope()
 
+    while True:
+        line = raw_input("scheme> ")
+
+        try:
+            ast = lexer.get_ast(lexer.tokenize(line))
+            scope.eval(ast)
+        except Exception as e:
+            print e
