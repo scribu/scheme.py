bbfc85752ea7f8e93f92e479fe3e91860c621340
scribu <mail@scribu.net>
2012-11-18 20:48:30 +0200

    auto-display result of each expression

--- repl.py
+++ repl.py
@@ -6,10 +6,12 @@ def start():
     while True:
         try:
             line = raw_input("scheme> ")
+            line = '(display %s)' % line
 
             try:
                 ast = lexer.get_ast(lexer.tokenize(line))
                 scope.eval(ast)
+                print
             except Exception as e:
                 print e
         except EOFError:
