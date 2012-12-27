acc3572d2fc147104b595cf4c21d3e3986d2f375
scribu <mail@scribu.net>
2012-12-27 10:31:38 +0200

    remove unnecessary main() function

--- scheme.py
+++ scheme.py
@@ -1,13 +1,9 @@
 import sys
 
-def main():
-    if len(sys.argv) > 1:
-        import core
+if len(sys.argv) > 1:
+    import core
 
-        core.proc_load(core.GlobalScope(), sys.argv[1])
-    else:
-        from repl import REPL
-        REPL().start()
-
-if __name__=="__main__":
-    main()
+    core.proc_load(core.GlobalScope(), sys.argv[1])
+else:
+    from repl import REPL
+    REPL().start()
